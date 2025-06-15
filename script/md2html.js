26cfb72efa193b6ba2f69a1564a8f396c371e2f3
shaobai-li <shaobai.ai@gmail.com>
2025-06-16 07:37:30 +0800

    feat: 引入puppeteer模块把HTML转换为png

--- script/md2html.js
+++ script/md2html.js
@@ -1,26 +1,30 @@
 import { readFileSync, writeFileSync } from 'fs';
 import { marked } from 'marked';
+import puppeteer from 'puppeteer';
 
 // Get markdown file path from command line arguments
 const mdFile = process.argv[2];
 const htmlFile = process.argv[3];
-
 if (!mdFile) {
     console.error('Please provide a markdown file path as argument');
     process.exit(1);
 }
-
 if (!htmlFile) {
     htmlFile = mdFile.replace(/\.md$/, '.html');
 }
+const pngFile = htmlFile.replace(/\.html$/, '.png');
 
 const md = readFileSync(mdFile, 'utf-8');
 const html = marked.parse(md);
-
 const fullHtml = `<!DOCTYPE html><html><body>${html}</body></html>`;
 
-// console.log(html);
-
-
-writeFileSync(htmlFile, fullHtml);
-console.log(`done: ${htmlFile}`);
+puppeteer.launch().then(async (browser) => {
+    const page = await browser.newPage();
+    await page.setContent(fullHtml);
+    await page.screenshot({ path: pngFile });
+    await browser.close();
+    console.log(`done: ${pngFile}`);
+}).catch(err => {
+    console.error('❌ Failed to render PNG:', err);
+    process.exit(1);
+});
