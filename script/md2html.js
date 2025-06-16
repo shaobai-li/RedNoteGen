1efe8dea31b89f5ce6cc563f9cd5df3b753c3d99
shaobai-li <shaobai.ai@gmail.com>
2025-06-16 22:45:32 +0800

    feat: 增加引入css样式的功能

--- script/md2html.js
+++ script/md2html.js
@@ -4,19 +4,17 @@ import puppeteer from 'puppeteer';
 
 // Get markdown file path from command line arguments
 const mdFile = process.argv[2];
-const htmlFile = process.argv[3];
+const cssFile = process.argv[3];
 if (!mdFile) {
     console.error('Please provide a markdown file path as argument');
     process.exit(1);
 }
-if (!htmlFile) {
-    htmlFile = mdFile.replace(/\.md$/, '.html');
-}
-const pngFile = htmlFile.replace(/\.html$/, '.png');
+const pngFile = mdFile.replace(/\.md$/, '.png');
 
 const md = readFileSync(mdFile, 'utf-8');
 const html = marked.parse(md);
-const fullHtml = `<!DOCTYPE html><html><body>${html}</body></html>`;
+const css = readFileSync(cssFile, 'utf-8');
+const fullHtml = `<!DOCTYPE html><html><head><style>${css}</style></head><body>${html}</body></html>`;
 
 puppeteer.launch().then(async (browser) => {
     const page = await browser.newPage();
