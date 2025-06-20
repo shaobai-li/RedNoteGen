fb5455605d92eca7a4b50620089e221c30cbdc3d
shaobai-li <shaobai.ai@gmail.com>
2025-06-20 23:48:46 +0800

    feat: 新增命令行参数以手动设置HTML高度

--- script/md2html.js
+++ script/md2html.js
@@ -5,6 +5,8 @@ import puppeteer from 'puppeteer';
 // Get markdown file path from command line arguments
 const mdFile = process.argv[2];
 const cssFile = process.argv[3];
+const height = parseInt(process.argv[4]) || 2000; // Default height is 2000
+
 if (!mdFile) {
     console.error('Please provide a markdown file path as argument');
     process.exit(1);
@@ -21,7 +23,7 @@ console.log(`Generated HTML: ${htmlFile}`);
 
 puppeteer.launch().then(async (browser) => {
     const page = await browser.newPage();
-    await page.setViewport({ width: 1000, height: 2000 });
+    await page.setViewport({ width: 1000, height: height });
     await page.setContent(fullHtml);
     await page.screenshot({ path: pngFile });
     await browser.close();
